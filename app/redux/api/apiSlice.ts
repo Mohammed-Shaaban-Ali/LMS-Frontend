0ff2f170a0fe67e3abc2fde111697361ef087b21
Mohammed-Shaaban-Ali <ms7500746@gmail.com>
2024-06-13 07:57:15 +0300

    add refrech token

--- app/redux/api/apiSlice.ts
+++ app/redux/api/apiSlice.ts
@@ -1,8 +1,39 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { userLogin } from "../features/auth/authSlice";
 export const apiSlice = createApi({
   reducerPath: "pokemonApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000/api/" }),
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    refrechToken: builder.query({
+      query: (data) => ({
+        url: "refresh",
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+
+    loadUser: builder.query({
+      query: (data) => ({
+        url: "get-login-user",
+        method: "GET",
+        credentials: "include" as const,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          // console.log(result.data?.activationToken);
+          dispatch(
+            userLogin({
+              token: result.data?.activationToken,
+              user: result.data?.user,
+            })
+          );
+        } catch (error: any) {
+          console.log(error);
+        }
+      },
+    }),
+  }),
 });
 
-export const {} = apiSlice;
+export const { useLazyRefrechTokenQuery, useLoadUserQuery } = apiSlice;
