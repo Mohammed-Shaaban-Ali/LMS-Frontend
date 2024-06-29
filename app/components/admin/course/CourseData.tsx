9cb2f12721f2c47dd880d44f24208f0e4b60a60a
Mohammed-Shaaban-Ali <ms7500746@gmail.com>
2024-06-29 22:37:13 +0300

    NxteAndPrevButtons Component

--- app/components/admin/course/CourseData.tsx
+++ app/components/admin/course/CourseData.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { AddCircle, Delete } from "@mui/icons-material";
 import toast from "react-hot-toast";
+import NxteAndPrevButtons from "./NxteAndPrevButtons";
 
 type Props = {
   active: number;
@@ -133,55 +134,7 @@ function CourseData({
         />
       </div>
       <br />
-      <div className="w-full flex items-center justify-between">
-        <button
-          onClick={prevOptions}
-          className="relative inline-flex items-center px-12 py-2 overflow-hidden text-lg font-medium text-[#37a39a] border-2 border-[#37a39a] rounded-2xl hover:text-white group hover:bg-gray-50"
-        >
-          <span className="absolute left-0 block w-full h-0 transition-all bg-[#37a39a] opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
-          <span className="absolute left-0 flex items-center justify-start w-10 h-10 duration-300 transform -translate-x-full group-hover:translate-x-0 ease">
-            <svg
-              className="w-5 h-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M10 19l-7-7m0 0l7-7m-7 7h18"
-              ></path>
-            </svg>
-          </span>
-          <span className="relative">Prev</span>
-        </button>
-
-        <button
-          onClick={nextOptions}
-          className="relative inline-flex items-center px-12 py-2 overflow-hidden text-lg font-medium text-[#37a39a] border-2 border-[#37a39a] rounded-2xl hover:text-white group hover:bg-gray-50"
-        >
-          <span className="absolute left-0 block w-full h-0 transition-all bg-[#37a39a] opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
-          <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
-            <svg
-              className="w-5 h-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M14 5l7 7m0 0l-7 7m7-7H3"
-              ></path>
-            </svg>
-          </span>
-          <span className="relative">Next</span>
-        </button>
-      </div>
+      <NxteAndPrevButtons prevOptions={prevOptions} nextOptions={nextOptions} />
     </div>
   );
 }
